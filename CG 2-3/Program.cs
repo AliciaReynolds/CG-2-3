83511ae753c1a11f5eb7ae06b485db4c0ef67239
Alicia Reynolds <alicia.a.wilson@gmail.com>
2018-07-19 22:35:45 -0500

    Add text to string

--- CG 2-3/Program.cs
+++ CG 2-3/Program.cs
@@ -6,7 +6,21 @@ namespace CG_2_3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Coder Mad Libs!!");
+            Console.WriteLine();
+            Console.WriteLine("Please Type an Adjective.");
+            string adjective = Console.ReadLine();
+
+            Console.WriteLine("Please Type a Verb.");
+            string verb = Console.ReadLine();
+
+            Console.WriteLine("Please Type a Noun.");
+            string noun = Console.ReadLine();
+
+            Console.WriteLine("On " + adjective + " days I like to " + verb + " while I play my " + noun);
+            Console.ReadLine();
+
+
         }
     }
 }
